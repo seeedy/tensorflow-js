7575826cba7e78ae36dbdd72450e10b302b6fd60
Christian Siedentop <40824622+seeedy@users.noreply.github.com>
2020-02-12 00:50:04 +0100

    adding webpack

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.get('/btc', (req, res) => {
+    const url =
+        'https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-01-01&end=2020-01-01';
+    axios.get(url).then(response => {
+        const { data } = response;
+        res.json(data);
+    });
+});
+
+app.listen(6969, () => console.log('serving on port 6969 '));
--- script.js
+++ script.js
@@ -1,87 +1,55 @@
+const axios = require('axios');
+
 async function getData() {
-    const data = await fetch(
-        'https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-01-01&end=2020-01-01'
-    ).then(res => res.json());
-    return data;
-}
-
-async function prepareData() {
-    const data = await getData();
+    const data = await axios
+        .get(
+            'https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-01-01&end=2020-01-01'
+        )
+        .then(res => res.json());
     console.log(data);
 
-    const { bpi } = data;
-    const days = Object.keys(bpi);
-    const values = Object.values(bpi);
-
-    let prepared = [];
-    for (let i = 0; i <= days.length; i++) {
-        prepared.push({ date: days[i], price: values[i] });
-    }
-    console.log(prepared);
-
-    tfvis.render.scatterplot(
-        { name: 'BTCUSD' },
-        { values },
-        {
-            xLabel: 'date',
-            yLabel: '$',
-            height: 300
-        }
-    );
-}
-
-prepareData();
-
-/* http.get(
-    {
-        host: 'api.coindesk.com',
-        path: '/v1/bpi/historical/close.json?start=2019-01-01&end=2020-01-01'
-    },
-    res => {
-        // Continuously update stream with data
-        var body = '';
-        res.on('data', chunk => {
-            body += chunk;
-        });
-        res.on('end', function() {
-            // Data reception is done, do whatever with it!
-            var parsed = JSON.parse(body);
-            console.log(parsed);
-            const { bpi } = parsed;
-            const days = Object.keys(bpi);
-            const values = Object.values(bpi);
-            let data = [];
-            for (let i = 0; i <= days.length; i++) {
-                data.push({ date: days[i], price: values[i] });
+    /* function computeSMA(data, window) {
+        console.log(data);
+        let r_avgs = [];
+        let avg_prev = 0;
+    
+        for (let i = 0; i <= Object.keys(data).length - window; i++) {
+            let curr_avg = 0.0;
+            let t = i + window;
+    
+            for (let j = i; j < t && j <= Object.keys(data).length; j++) {
+                curr_avg += data[j].price / window;
             }
-            console.log(data);
-            const window = 50;
-
-            const simpleMA = computeSMA(data, window);
-            console.log(simpleMA);
-        });
-    }
-);
-
-function computeSMA(data, window) {
-    console.log(data);
-    let r_avgs = [];
-    let avg_prev = 0;
-
-    for (let i = 0; i <= Object.keys(data).length - window; i++) {
-        let curr_avg = 0.0;
-        let t = i + window;
-
-        for (let j = i; j < t && j <= Object.keys(data).length; j++) {
-            curr_avg += data[j].price / window;
+            r_avgs.push({
+                set: data.slice(i, i + window),
+                date: data[i].date,
+                avg: curr_avg
+            });
+            avg_prev = curr_avg;
         }
-        r_avgs.push({
-            set: data.slice(i, i + window),
-            date: data[i].date,
-            avg: curr_avg
-        });
-        avg_prev = curr_avg;
+        return r_avgs;
+    } */
+
+    async function cleanData() {
+        const data = await getData();
+        const { bpi } = data;
+        console.log(Obj.entries(bpi));
+        const days = Object.keys(bpi);
+
+        let cleaned = [];
+        /* for (let i = 0; i <= days.length; i++) {
+            cleaned.push(;
+        } */
+        console.log(cleaned);
+
+        tfvis.render.scatterplot(
+            { name: 'BTCUSD' },
+            { values },
+            {
+                xLabel: 'date',
+                yLabel: '$',
+                height: 300
+            }
+        );
     }
-    return r_avgs;
 }
- */
