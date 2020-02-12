ca653bc5915e82a9ad08d88544be6b27dded6a22
Chris <christian.siedentop@xailabs.de>
2020-02-12 18:10:14 +0100

    calculating SMA and plotting to line chart working

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.get('/', (req, res) => {
     res.send('Hello');
 });
 
-app.get('/btc', (req, res) => {
+/* app.get('/btc', (req, res) => {
     const url =
         'https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-01-01&end=2020-01-01';
     axios.get(url).then(response => {
         const { data } = response;
         res.json(data);
     });
-});
+}); */
 
 app.listen(6969, () => console.log('serving on port 6969 '));
--- script.js
+++ script.js
@@ -1,55 +1,86 @@
-const axios = require('axios');
+import '@babel/polyfill';
+import axios from 'axios';
+import * as tf from '@tensorflow/tfjs';
+import * as tfvis from '@tensorflow/tfjs-vis';
 
 async function getData() {
-    const data = await axios
-        .get(
-            'https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-01-01&end=2020-01-01'
-        )
-        .then(res => res.json());
-    console.log(data);
-
-    /* function computeSMA(data, window) {
-        console.log(data);
-        let r_avgs = [];
-        let avg_prev = 0;
-    
-        for (let i = 0; i <= Object.keys(data).length - window; i++) {
-            let curr_avg = 0.0;
-            let t = i + window;
-    
-            for (let j = i; j < t && j <= Object.keys(data).length; j++) {
-                curr_avg += data[j].price / window;
-            }
-            r_avgs.push({
-                set: data.slice(i, i + window),
-                date: data[i].date,
-                avg: curr_avg
-            });
-            avg_prev = curr_avg;
-        }
-        return r_avgs;
-    } */
-
-    async function cleanData() {
-        const data = await getData();
-        const { bpi } = data;
-        console.log(Obj.entries(bpi));
-        const days = Object.keys(bpi);
-
-        let cleaned = [];
-        /* for (let i = 0; i <= days.length; i++) {
-            cleaned.push(;
-        } */
-        console.log(cleaned);
-
-        tfvis.render.scatterplot(
-            { name: 'BTCUSD' },
-            { values },
-            {
-                xLabel: 'date',
-                yLabel: '$',
-                height: 300
-            }
-        );
-    }
+    console.log('getting data');
+    const data = await axios.get(
+        'https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-01-01&end=2020-01-01'
+    );
+    return data;
 }
+
+async function cleanData() {
+    const data = await getData();
+    const { bpi } = data.data;
+    const values = Object.values(bpi);
+    const days = Object.keys(bpi);
+
+    const rawData = days.map(day => {
+        const index = days.indexOf(day);
+        return { x: day, y: values[index] };
+    });
+    console.log({ rawData });
+
+    const series1 = Array(100)
+        .fill(0)
+        .map(y => Math.random() * 100 - Math.random() * 150)
+        .map((y, x) => ({ x, y }));
+    const series2 = Array(100)
+        .fill(0)
+        .map(y => Math.random() * 100 - Math.random() * 150)
+        .map((y, x) => ({ x, y }));
+
+    const smaData = getSMAData(rawData, 50);
+    console.log({ smaData });
+
+    const surface = { name: 'Chart', tab: 'Charts' };
+
+    tfvis.render.linechart(
+        surface,
+        { values: [rawData, smaData] },
+        { xLabel: 'days' }
+    );
+    /* tfvis.render.linechart(
+        { name: 'SMA', tab: 'Charts' },
+        { values: smaData },
+        { xLabel: 'days' }
+    ); */
+}
+
+function getSMAData(data, period) {
+    const dates = data.map(d => d.x);
+    const values = data.map(d => d.y);
+    console.log(values);
+
+    let avgs = [];
+    function calculateSMA(avg) {
+        avgs.push(avg);
+        if (avgs.length > period) {
+            avgs.splice(0, 1);
+        }
+
+        let sum = 0;
+
+        for (var i in avgs) {
+            sum += avgs[i];
+        }
+
+        let n = period;
+        if (avgs.length < period) {
+            n = avgs.length;
+        }
+        return sum / n;
+    }
+
+    let smaData = [];
+    for (var i in values) {
+        var n = values[i];
+        var sma = calculateSMA(n);
+        smaData.push({ x: dates[i], y: sma });
+    }
+    return smaData;
+}
+
+document.addEventListener('DOMContentLoaded', cleanData);
